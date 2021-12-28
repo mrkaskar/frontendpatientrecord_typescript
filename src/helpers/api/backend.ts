4be99ef0d2ab823b706ddadd9beef0a48357f420
Alphakas <alphakas@Alphakass-iMac.local>
2021-12-28 19:30:39 +0530

    change backend

--- src/helpers/api/backend.ts
+++ src/helpers/api/backend.ts
@@ -1,3 +1,3 @@
-export const baseURL = process.env.BACKEND || 'http://localhost:5000';
+export const baseURL = process.env.BACKEND || 'https://shwelawon.herokuapp.com';
 // export const baseURL = 'http://localhost:5000';
 export const url = `${baseURL}/api`;
